32c4e4ef44c5456b2a22593b053873ec4d2cfbd2
Łukasz <strzalkowski.lukasz@gmail.com>
2018-06-10 13:21:51 +0200

    setting up users menu

--- src/main/java/controller/Menu.java
+++ src/main/java/controller/Menu.java
@@ -4,6 +4,8 @@ import dao.UserDao;
 import model.User;
 
 import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Menu {
 
@@ -14,7 +16,79 @@ public class Menu {
 
     public void wyswietlMenu() throws SQLException {
         UserDao userDao = new UserDao();
+        Scanner scanner = new Scanner(System.in);
+        String chooseOption;
 
-        userDao.save(new User("Łukasz", "Strzałkowski", "test@aa.pl"));
+        System.out.println("1- add new user to data base");
+        System.out.println("2- delete user from data base");
+        System.out.println("3- update user's data");
+        System.out.println("4- find user by ID number");
+        System.out.println("5- show all users");
+        System.out.println("6- quit application");
+
+        do {
+        System.out.println("Choose your option: ");
+        chooseOption = scanner.next();
+
+
+            switch (chooseOption) {
+                case "1":
+                    System.out.print("Enter new user's first name: ");
+                    String firstName = scanner.next();
+                    System.out.print("Enter new user's last name: ");
+                    String lastName = scanner.next();
+                    System.out.print("Enter new user's last name: ");
+                    String email = scanner.next();
+
+                    userDao.save(new User(firstName, lastName, email));
+
+                    break;
+                case "2":
+                    System.out.print("Enter ID number of user who you want to delete: ");
+                    int id = scanner.nextInt();
+                    userDao.delete(id);
+
+                    break;
+                case "3":
+                    System.out.print("Enter user's ID whose data you want to change: ");
+                    id = scanner.nextInt();
+                    System.out.print("Enter user's new first name: ");
+                    firstName = scanner.next();
+                    System.out.print("Enter user's new last name: ");
+                    lastName = scanner.next();
+                    System.out.print("Enter user's new e-mail: ");
+                    email = scanner.next();
+
+                    userDao.update(new User(id, firstName, lastName, email));
+
+                    break;
+                case "4":
+                    System.out.print("Enter ID number of searching user:  ");
+                    id = scanner.nextInt();
+                    userDao.findById(id);
+
+                    break;
+                case "5":
+                    System.out.println("List of all users: ");
+
+                    userDao.findAll();
+
+                    Iterator<User> it = userDao.getList().iterator();
+
+                    while (it.hasNext()) {
+                        User user = it.next();
+                        System.out.println(user);
+                    }
+
+                    break;
+                case "q":
+                case "Q":
+                    System.out.println("Thank You");
+                    System.out.println("@ Lucjan 2018");
+
+                    break;
+            }
+        } while (!chooseOption.equalsIgnoreCase("Q"));
+
     }
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -3,9 +3,12 @@ package dao;
 import config.Database;
 import model.User;
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
@@ -15,8 +18,12 @@ public class UserDao {
     //delete
     //find by id
     //find all
-
+    List<User> list = new ArrayList<>();
     private Database database = new Database();
+
+    public List<User> getList() {
+        return list;
+    }
 
     public User save(User user) throws SQLException {
         //insert
@@ -47,19 +54,67 @@ public class UserDao {
         //metoda save nie musi właściwie nic zwracać- może być voidem.
     }
 
-    public void update(User user) {
+    public void update(User user) throws SQLException {
+        Connection connection = database.getConnection();
+        String sql = "UPDATE user SET first_name = ?, last_name = ?, email = ? WHERE id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
 
+        statement.setString(1, user.getFirstName());
+        statement.setString(2, user.getLastName());
+        statement.setString(3, user.getEmail());
+        statement.setInt(4, user.getId());
+
+        statement.executeUpdate();
     }
 
-    public void delete(int id) {
+    public void delete(int id) throws SQLException {
+        Connection connection = database.getConnection();
+        String sql = "DELETE FROM user WHERE id = ?";
 
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        statement.setInt(1, id);
+
+        statement.executeUpdate();
     }
 
-    public User findById(int id) {
-    return null;
+    public User findById(int id) throws SQLException {
+        Connection connection = database.getConnection();
+        String sql = "SELECT id, first_name, last_name, email FROM user WHERE id = ?";
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        statement.setInt(1, id);
+        ResultSet result = statement.executeQuery();
+        User user = null;
+
+        while (result.next()){
+            id = result.getInt("id");
+            String firstName = result.getString("first_name");
+            String lastName = result.getString("last_name");
+            String email = result.getString("email");
+            user = new User(id, firstName, lastName, email);
+        }
+        return user;
     }
 
-    public List<User> findAll() {
-    return null;
+    public List<User> findAll() throws SQLException {
+        Connection connection = database.getConnection();
+        String sql = "SELECT id, first_name, last_name, email FROM user";
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        ResultSet result = statement.executeQuery();
+        User user = null;
+        list.clear();
+        while (result.next()){
+            int id = result.getInt("id");
+            String firstName = result.getString("first_name");
+            String lastName = result.getString("last_name");
+            String email = result.getString("email");
+            user = new User(id, firstName, lastName, email);
+            list.add(user);
+        }
+        return list;
     }
 }
