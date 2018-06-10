90e45c342ea0b087009dce02c85b684e7b3ed9da
Łukasz <strzalkowski.lukasz@gmail.com>
2018-06-10 11:53:32 +0200

    setting up voids to moderate data base

--- src/main/java/controller/Menu.java
+++ src/main/java/controller/Menu.java
@@ -1,4 +1,20 @@
 package controller;
 
+import dao.UserDao;
+import model.User;
+
+import java.sql.SQLException;
+
 public class Menu {
+
+    public static void main(String[] args) throws SQLException {
+        Menu menu = new Menu();
+        menu.wyswietlMenu();
+    }
+
+    public void wyswietlMenu() throws SQLException {
+        UserDao userDao = new UserDao();
+
+        userDao.save(new User("Łukasz", "Strzałkowski", "test@aa.pl"));
+    }
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,65 @@
 package dao;
 
+import config.Database;
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 public class UserDao {
+
+    //save
+    //update
+    //delete
+    //find by id
+    //find all
+
+    private Database database = new Database();
+
+    public User save(User user) throws SQLException {
+        //insert
+        //1. pobierz połączenia
+        Connection connection = database.getConnection();
+
+        //2. napisz zapytanie
+//        String sql = "INSERT INTO user(first_name, last_name, email) " +
+//                "VALUES ("+ user.getFirstName() + "," + user.getLastName() + "," + user.getEmail() + ")";
+                //nie trzeba łamać tego wiersza IntelliJ sam dodaje dwa stringi
+        //! tAK NIE ROBIMY ZE WZGLĘDU NA SQL INJECTIOP !"
+        String sql = "INSERT INTO user(first_name, last_name, email) VALUES (?,?,?)";
+
+        //3. utworz obiekt PreparedStatement
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+        //4. uzupełnij parametry zapytania
+        statement.setString(1, user.getFirstName());
+        statement.setString(2, user.getLastName());
+        statement.setString(3, user.getEmail());
+
+        //parametr index to kolejne znaki zapytania, wykorzystujemy wszędzie tam, gdzie podajemy dane
+
+        //5. wykonaj zapytanie w bazie
+        statement.executeUpdate();
+
+        return user;
+        //metoda save nie musi właściwie nic zwracać- może być voidem.
+    }
+
+    public void update(User user) {
+
+    }
+
+    public void delete(int id) {
+
+    }
+
+    public User findById(int id) {
+    return null;
+    }
+
+    public List<User> findAll() {
+    return null;
+    }
 }
